0b1a76a543ed8faa828509fde314dad2325d0cbe
princej69 <pjsatur@gmail.com>
2022-08-27 21:40:38 +0800

    Alpha 1.3

--- changelog.md
+++ changelog.md
@@ -1,4 +1,9 @@
 # Changelog
+## Alpha 1.3 [Aug 27, 2022]
+- Added save function because, martin talevski called it a fake game
+  - ~~I NOW HAVE FEW HOURS BECAUSE IF I DONT ADD, MARTIN TALEVSKI WILL BE SO MAD AT ME~~ I only did it, for **10 minutes** (probably 15)
+- Added multipliers
+- Fixed the NaN meters and 690 meters thing (if it was 6.90E-1, not the actual 690 000 000 micrometers).
 ## Alpha 1.2 [Aug 25, 2022]
 - Added Size / sec<sup>2</sup>
 - Added the check if you have sufficient things
--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 let game = {
     size: 1,
-    total: 0,
-    gain: 1,
+    total: 0.,
+    gain: 1.,
     // gain
-    persec: 0,
-    persec2: 0 //basically increases persec this amount per second.
+    persec: 0.,
+    persec2: 0., //basically increases persec this amount per second.
+
+    // multipliers
+    psmul: 1
+}
+
+function save() {
+    localStorage.setItem("SizeIncrementalSave", JSON.stringify(game))
+    alert(localStorage.getItem("SizeIncrementalSave"))
+}
+function load() {
+    if(localStorage.getItem("SizeIncrementalSave")) {
+        game = JSON.parse(localStorage.getItem("SizeIncrementalSave"))
+    }
 }
 
 function checkIfSufficientOrNot(selector, req, unoreverse = false) {
@@ -26,13 +39,20 @@ function checkIfSufficientOrNot(selector, req, unoreverse = false) {
 function checkSelectors() {
     checkIfSufficientOrNot('MSps', game.total >= 100)
     checkIfSufficientOrNot('MSps2', game.persec >= 1000)
+    checkIfSufficientOrNot('MuSp', game.persec >= 10000)
 }
 
 setInterval(() => {
-    document.getElementById("size").textContent = (game.size / (Math.pow(1000, (Math.floor(Math.log10(game.size)/3))))).toFixed(2);
-    document.getElementById("illion").textContent = getSIprefixes(Math.floor(Math.log10(game.size)/3));
-    game.size += game.persec/60
-    game.total += game.persec/60
+    if (game.size < 1) {
+        document.getElementById("size").textContent = Math.pow(10,Math.log10(game.size)%1).toFixed(2) + "E" + Math.floor(Math.log10(game.size))
+    }else{
+        document.getElementById("size").textContent = (game.size / (Math.pow(1000, (Math.floor(Math.log10(game.size)/3))))).toFixed(2);
+    }
+    document.getElementById("illion").textContent = getSIprefixes(Math.floor(Math.log10(game.size)/3))
+    game.size += (game.persec*game.psmul)/60
+    game.total += (game.persec*game.psmul)/60
     game.persec += game.persec2/60
     checkSelectors()
 },1000/60)
+
+load()
